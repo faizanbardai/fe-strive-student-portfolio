fd34b421a9f5e0d6e5db8b0905ee50e9e80aaf16
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-11 16:06:02 +0100

    added post student API

---
+++ src/API/PostStudent.jsx
@@ -0,0 +1,20 @@
+const baseURL = "http://localhost:3001/students";
+
+const PostStudents = async (student) => {
+  let URL = baseURL;
+  try {
+    let response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+       },
+      body: JSON.stringify(student)
+    });
+    if (response.ok) {
+      return await response.json();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default PostStudents;
